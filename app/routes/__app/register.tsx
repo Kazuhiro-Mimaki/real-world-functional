834aca0f96da52679969b704260e677996d88a03
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-02-25 17:59:06 +0900

    feat: complete register page

--- app/routes/__app/register.tsx
+++ app/routes/__app/register.tsx
@@ -1,39 +1,86 @@
 import { Form, Link, useActionData } from '@remix-run/react';
-import { json, redirect } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
 import type { ActionArgs } from '@remix-run/node';
-import { commitUserSession, createUserSession } from '~/server/user';
-import { ok } from 'neverthrow';
-import { createUser, saveUser, validateUser } from '~/models/user/register';
+import { commitUserSession, createUserSession, register } from '~/server/user';
+import { db } from '~/server/db.server';
+import { badRequest } from '~/server/utils';
+
+const validateUsername = (username: unknown) => {
+  if (typeof username !== 'string' || username.length < 3) {
+    return `Usernames must be at least 3 characters long`;
+  }
+};
+
+const validateEmail = (email: unknown) => {
+  if (typeof email !== 'string' || email.length < 6) {
+    return `Emails must be at least 6 characters long`;
+  }
+};
+
+const validatePassword = (password: unknown) => {
+  if (typeof password !== 'string' || password.length < 6) {
+    return `Passwords must be at least 6 characters long`;
+  }
+};
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
 
-  const input = {
-    kind: 'UnValidated' as const,
-    username: form.get('username'),
-    email: form.get('email'),
-    password: form.get('password'),
+  const username = form.get('username');
+  const email = form.get('email');
+  const password = form.get('password');
+
+  if (typeof username !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fields = { username, email, password };
+  const fieldErrors = {
+    username: validateUsername(username),
+    email: validateEmail(email),
+    password: validatePassword(password),
   };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
 
-  const result = await ok(input).andThen(validateUser).asyncAndThen(createUser).andThen(saveUser);
+  const userExists = await db.user.findFirst({
+    where: { username },
+  });
+  if (userExists) {
+    return badRequest({
+      fieldErrors: null,
+      fields,
+      formError: `User with username ${username} already exists`,
+    });
+  }
 
-  return result.match(
-    async (user) => {
-      const session = await createUserSession(user.id);
-      return redirect('/', {
-        headers: {
-          'Set-Cookie': await commitUserSession(session),
-        },
-      });
+  const user = await register({ username, email, password });
+  if (!user) {
+    return badRequest({
+      fieldErrors: null,
+      fields,
+      formError: `Something went wrong trying to create a new user.`,
+    });
+  }
+  const session = await createUserSession(user.id);
+  return redirect('/', {
+    headers: {
+      'Set-Cookie': await commitUserSession(session),
     },
-    async (error) => {
-      return json({ message: error.message });
-    }
-  );
+  });
 };
 
 export default function Register() {
-  const error = useActionData<typeof action>();
+  const actionData = useActionData<typeof action>();
 
   return (
     <div className='container flex flex-wrap flex-col space-y-8 items-center mx-auto pt-12'>
@@ -51,26 +98,54 @@ export default function Register() {
             name='username'
             placeholder='Username'
             type='text'
+            defaultValue={actionData?.fields?.username}
+            aria-invalid={Boolean(actionData?.fieldErrors?.username)}
+            aria-errormessage={actionData?.fieldErrors?.username && 'username-error'}
           />
+          {actionData?.fieldErrors?.username ? (
+            <p className='text-red-500' role='alert' id='username-error'>
+              {actionData.fieldErrors.username}
+            </p>
+          ) : null}
           <input
             className='rounded-md border focus:outline-none focus:ring-4 focus:ring-opacity-50 appearance-none text-gray-900 bg-gray-50 border-gray-300 focus:border-primary focus:ring-primary-300 px-4 py-2.5'
             name='email'
             placeholder='Email'
             type='text'
+            defaultValue={actionData?.fields?.email}
+            aria-invalid={Boolean(actionData?.fieldErrors?.email)}
+            aria-errormessage={actionData?.fieldErrors?.email && 'email-error'}
           />
+          {actionData?.fieldErrors?.email ? (
+            <p className='text-red-500' role='alert' id='email-error'>
+              {actionData.fieldErrors.email}
+            </p>
+          ) : null}
           <input
             className='rounded-md border focus:outline-none focus:ring-4 focus:ring-opacity-50 appearance-none text-gray-900 bg-gray-50 border-gray-300 focus:border-primary focus:ring-primary-300 px-4 py-2.5'
             name='password'
             placeholder='Password'
             type='password'
+            defaultValue={actionData?.fields?.password}
+            aria-invalid={Boolean(actionData?.fieldErrors?.password)}
+            aria-errormessage={actionData?.fieldErrors?.password && 'password-error'}
           />
+          {actionData?.fieldErrors?.password ? (
+            <p className='text-red-500' role='alert' id='password-error'>
+              {actionData.fieldErrors.password}
+            </p>
+          ) : null}
+
+          {actionData?.formError ? (
+            <p className='text-red-500' role='alert'>
+              {actionData.formError}
+            </p>
+          ) : null}
 
           <button type='submit' className='text-white bg-green-600 border-green-600 self-end px-5 py-2 rounded'>
             Sign up
           </button>
         </fieldset>
-
-        {error ? <em className='text-red-600'>{error.message}</em> : null}
       </Form>
     </div>
   );
