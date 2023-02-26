7dfaf72c9371b78278d870eabc75833f6e15624a
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-02-27 07:21:06 +0900

    feat: branded type

--- app/models/baseTypes.ts
+++ app/models/baseTypes.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod';
 
 /**
+ * Branded type
+ */
+export type Branded<T, K> = T & { __brand: K };
+
+/**
  * Constrained to be 5 chars or less, not null
  */
 export const String5 = z.string().min(5);
-export type String5 = z.infer<typeof String5>;
+export type String5 = Branded<z.infer<typeof String5>, 'String5'>;
 
 /**
  * An email address
  */
 export const Email = z.string().email();
-export type Email = z.infer<typeof Email>;
+export type Email = Branded<z.infer<typeof Email>, 'Email'>;
--- app/models/user/vo.ts
+++ app/models/user/vo.ts
@@ -1,31 +1,32 @@
 import type { Result } from 'neverthrow';
 import { err, ok } from 'neverthrow';
 import type { ZodError } from 'zod';
+import type { Branded } from '../baseTypes';
 import { String5, Email } from '../baseTypes';
 
 /**
  * UserName
  */
-export type UserName = String5;
-export const UserName = (input: string): Result<string, ZodError<string>> => {
+export type UserName = Branded<string, 'UserName'>;
+export const UserName = (input: string): Result<UserName, ZodError<string>> => {
   const parsed = String5.safeParse(input);
-  return parsed.success ? ok(parsed.data) : err(parsed.error);
+  return parsed.success ? ok(parsed.data as UserName) : err(parsed.error);
 };
 
 /**
  * An email address
  */
-export type EmailAddress = Email;
-export const EmailAddress = (input: string): Result<string, ZodError<string>> => {
+export type EmailAddress = Branded<string, 'EmailAddress'>;
+export const EmailAddress = (input: string): Result<EmailAddress, ZodError<string>> => {
   const parsed = Email.safeParse(input);
-  return parsed.success ? ok(parsed.data) : err(parsed.error);
+  return parsed.success ? ok(parsed.data as EmailAddress) : err(parsed.error);
 };
 
 /**
  * An password
  */
-export type Password = String5;
-export const Password = (input: string): Result<string, ZodError<string>> => {
+export type Password = Branded<string, 'Password'>;
+export const Password = (input: string): Result<Password, ZodError<string>> => {
   const parsed = String5.safeParse(input);
-  return parsed.success ? ok(parsed.data) : err(parsed.error);
+  return parsed.success ? ok(parsed.data as Password) : err(parsed.error);
 };
