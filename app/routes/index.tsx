28f94fe4f1f416028579e43bd473333f7b44696e
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-03-12 11:02:10 +0900

    feat: layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import { Header } from '~/components';
 import { listArticles } from '~/modules/articles/repository';
 import { getUserId } from '~/server/user';
@@ -36,19 +36,26 @@ export default function Index() {
   return (
     <>
       <Header />
-      <div className='container mx-auto my-8'>
-        <section>
+      <div className='flex container mx-auto my-8 gap-x-8'>
+        <section className='w-3/4'>
           <section>global feed</section>
 
           <section className='divide-y divide-slate-200'>
             {articles.map((article) => (
               <article key={article.id} className='flex flex-col py-4'>
-                <span>{article.author.username}</span>
+                <Link to='/settings' className='text-green-500 hover:underline'>
+                  {article.author.username}
+                </Link>
                 <h1 className='text-2xl font-semibold'>{article.title}</h1>
-                <p className='text-base font-light text-gray-500'>{article.content}</p>
+                <p className='text-base font-light text-gray-500'>{article.content.substring(0, 500)}...</p>
               </article>
             ))}
           </section>
+        </section>
+
+        <section className='bg-slate-100 w-1/4 h-fit'>
+          <h1>Popular Tags</h1>
+          {/* TODO: list popular tags */}
         </section>
       </div>
     </>
