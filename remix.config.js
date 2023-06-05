08b9440538d30ba02fd9a4779ede8553e1591f7c
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-06-05 17:52:48 +0900

    fix: directory design

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,8 @@ module.exports = {
   ignoredRouteFiles: ['**/.*'],
   future: {
     unstable_tailwind: true,
+    v2_routeConvention: true,
+    v2_errorBoundary: true,
   },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
